cefabbe2c3909bbd92ec9677092413470846b076
Simone Poggiali <17195702+gibbok@users.noreply.github.com>
2022-03-25 23:02:32 +0100

    Application state and rendering cwd items (#2)

--- TODO.md
+++ TODO.md
@@ -16,6 +16,11 @@
 - [] User can select and navigate a folder by pressing `l` 
 - [] User can select a parent folder by pressing `h`
 - [] User can see the path of the current folder or item selected when navigating
-- [] User can select an item with return key when pressed the program exits returning selected item path
+- [] User can select and open an item with the `return` key
+- [] User can exit the program and navigate to selected directory, by pressing `!`
+- [] User can exit the program returning the path for the selected item using `space` 
 - [] User can exit the program by pressing `q`
 - [] User can see a list of files updated if the items changed
+
+## Resources
+https://iq.opengenus.org/chdir-fchdir-getcwd-in-c/#:~:text=The%20getcwd()%20function%20places,of%20getcwd()%20is%20undefined.
--- src/jjj.c
+++ src/jjj.c
@@ -1,7 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include "jjj.h"
+
+// Get current working directory
+void get_cwd(struct AppState *state)
+{
+    char cwd[256]; // FIXME: find a way to make it platform indipendent
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+    {
+        perror("getcwd() error");
+    }
+    else
+    {
+        strcpy(state->cwd, cwd);
+    }
+}
+
+// Print to screen results
+void print_cwd(struct AppState *state)
+{
+    printf("%s\n", state->cwd);
+}
+
+void print_dir_items_total(struct AppState *state)
+{
+    printf("Total: %i\n", state->dir_entries_total);
+}
+
+void print_dir_items(struct AppState *state)
+{
+    int i;
+    for (i = 0; i < state->dir_entries_total; ++i)
+    {
+        struct DirItem item = state->dir_entries[i];
+        if (item.is_dir == 1)
+            printf("%s/\n", item.name);
+        else
+            printf("%s\n", item.name);
+    }
+}
+
+void print_screen(struct AppState *state)
+{
+    system("clear");
+    print_cwd(state);
+    print_dir_items_total(state);
+    print_dir_items(state);
+};
+
+// List directory in application state
+void list_dir(struct AppState *state)
+{
+    struct dirent *dir_entry;
+
+    DIR *dr = opendir(state->cwd);
+
+    if (dr == NULL)
+        printf("Could not open current directory");
+
+    int i = 0;
+    while ((dir_entry = readdir(dr)) != NULL)
+    {
+        struct DirItem item = {};
+        strcpy(item.name, dir_entry->d_name);
+        if (dir_entry->d_type == DT_DIR)
+            item.is_dir = 1;
+        else
+            item.is_dir = 0;
+
+        state->dir_entries[i] = item;
+        ++i;
+        state->dir_entries_total = i;
+    }
+
+    closedir(dr);
+}
 
 int main()
 {
-    printf("Hello, World!");
+    struct AppState app_state = {};
+
+    get_cwd(&app_state);
+
+    list_dir(&app_state);
+
+    print_screen(&app_state);
+
     return 0;
 }
