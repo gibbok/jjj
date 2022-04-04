cae6d2422c3861b367f02477ca6ee38fe9e59048
Simone Poggiali <17195702+gibbok@users.noreply.github.com>
2022-04-04 17:24:47 +0200

    Organize files (#3)
    * Move file and add headers
    
    * Fixing missing import
    
    * Arrange includes
    
    * rename

---
+++ src/header/render.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "header/global.h"
+
+// Print to screen results
+void render_cwd(struct AppState *state)
+{
+    printf("%s\n", state->cwd);
+}
+
+void render_dir_items_total(struct AppState *state)
+{
+    printf("Total: %i\n", state->dir_entries_total);
+}
+
+void print_dir_items(struct AppState *state)
+{
+    int i;
+    for (i = 0; i < state->dir_entries_total; ++i)
+    {
+        struct DirItem item = state->dir_entries[i];
+        if (item.is_dir == 1)
+            printf("%s/\n", item.name);
+        else
+            printf("%s\n", item.name);
+    }
+}
+
+void render_screen(struct AppState *state)
+{
+    system("clear");
+    render_cwd(state);
+    render_dir_items_total(state);
+    print_dir_items(state);
+};
---
+++ src/items.c
@@ -0,0 +1,43 @@
+#include "header/global.h"
+
+// Get current working directory
+void get_cwd(struct AppState *state)
+{
+    char cwd[256]; // FIXME: find a way to make it platform indipendent
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+    {
+        perror("getcwd() error");
+    }
+    else
+    {
+        strcpy(state->cwd, cwd);
+    }
+}
+
+// List directory in application state
+void list_dir(struct AppState *state)
+{
+    struct dirent *dir_entry;
+
+    DIR *dr = opendir(state->cwd);
+
+    if (dr == NULL)
+        printf("Could not open current directory");
+
+    int i = 0;
+    while ((dir_entry = readdir(dr)) != NULL)
+    {
+        struct DirItem item = {};
+        strcpy(item.name, dir_entry->d_name);
+        if (dir_entry->d_type == DT_DIR)
+            item.is_dir = 1;
+        else
+            item.is_dir = 0;
+
+        state->dir_entries[i] = item;
+        ++i;
+        state->dir_entries_total = i;
+    }
+
+    closedir(dr);
+}
