be2e0f5cedb05cbb4fe2d308daea10ec5ca966d6
GibboK <gibbok.coding@gmail.com>
2022-04-06 16:27:25 +0200

    save

--- src/jjj.c
+++ src/jjj.c
@@ -5,15 +5,31 @@
 int main()
 {
     initscr();
+    clear();
+    noecho();
+    cbreak();
 
-    struct AppState app_state = {};
-
-    get_cwd(&app_state);
-    list_dir(&app_state);
-    render_screen(&app_state);
-
-    getch();
+    window();
+    detect_mouse();
+    clrtoeol();
+    refresh();
     endwin();
-
     return 0;
 }
+
+// MY CODE
+// int main()
+// {
+//     initscr();
+
+//     struct AppState app_state = {};
+
+//     get_cwd(&app_state);
+//     list_dir(&app_state);
+//     render_screen(&app_state);
+
+//     getch();
+//     endwin();
+
+//     return 0;
+// }
--- src/render.c
+++ src/render.c
@@ -37,3 +37,89 @@ void render_screen(struct AppState *state)
     print_dir_items(state);
     refresh();
 };
+
+#define WIDTH 30
+#define HEIGHT 10
+
+WINDOW *menu_win;
+int highlight = 1;
+int choice = 0;
+int c;
+int startx = 0;
+int starty = 0;
+
+char *choices[] = {
+    "Choice 1",
+    "Choice 2",
+    "Choice 3",
+    "Choice 4",
+    "Exit",
+};
+int n_choices = sizeof(choices) / sizeof(char *);
+void print_menu(WINDOW *menu_win, int highlight);
+
+void window()
+{
+    menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+    keypad(menu_win, TRUE);
+    refresh();
+    print_menu(menu_win, highlight);
+}
+
+void print_menu(WINDOW *menu_win, int highlight)
+{
+    int x, y, i;
+
+    x = 2;
+    y = 2;
+    box(menu_win, 0, 0);
+    for (i = 0; i < n_choices; ++i)
+    {
+        if (highlight == i + 1) /* High light the present choice */
+        {
+            wattron(menu_win, A_REVERSE);
+            mvwprintw(menu_win, y, x, "%s", choices[i]);
+            wattroff(menu_win, A_REVERSE);
+        }
+        else
+            mvwprintw(menu_win, y, x, "%s", choices[i]);
+        ++y;
+    }
+    wrefresh(menu_win);
+}
+
+void detect_mouse()
+{
+    while (1)
+    {
+        c = wgetch(menu_win);
+        switch (c)
+        {
+        case KEY_UP:
+            if (highlight == 1)
+                highlight = n_choices;
+            else
+                --highlight;
+            break;
+        case KEY_DOWN:
+            if (highlight == n_choices)
+                highlight = 1;
+            else
+                ++highlight;
+            break;
+        case 10:
+            choice = highlight;
+            break;
+        default:
+            mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+            refresh();
+            break;
+        }
+        print_menu(menu_win, highlight);
+        if (choice != 0) /* User did a choice come out of the infinite loop */
+            break;
+    }
+    mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+    clrtoeol();
+    refresh();
+}
